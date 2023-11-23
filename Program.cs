0d01735701c6db93e91569f68e5368008ae29a60
Olga_Kalinichenko <okalinichenko@icloud.com>
2023-11-23 12:12:06 +0200

    Added method NewThreeSymbolArray

--- Program.cs
+++ Program.cs
@@ -2,3 +2,18 @@
 // строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с 
 // клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется 
 // пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] NewThreeSymbolArray (string[] givenArray)
+{
+    for (int i = 0, j = 0; i < givenArray.Length; i++)
+    {
+        string checkedSymbol = givenArray[i];
+        string[] newArray = new string[j];
+        if (checkedSymbol.Length <= 3)
+        {
+            newArray[j] = checkedSymbol;
+            j++;
+        }
+    }
+    return newArray;
+}
